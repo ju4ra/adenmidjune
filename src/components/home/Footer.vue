6b79612624822c8e04894ed2de075e1891a68411
JU4RA <donnysuhend@gmail.com>
2025-01-07 14:26:06 +0700

    Add files via upload

---
+++ src/components/home/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="mt-8 flex justify-center items-center dark:text-white">
+    <p>© 2024 Islamic App. Crafted with ❤️ by JU4RA</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
